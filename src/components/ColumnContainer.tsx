6f3b91e80557bddbbe56acc2deef2fea280f2fae
Prithivi <prithivi@users.noreply.github.com>
2026-02-13 00:54:12 +0545

    feat: enhance ColumnContainer with editable title and task display

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,4 +1,4 @@
-import type { Column, Id } from "@/types";
+import type { Column, Id, Task } from "@/types";
 import {
   Card,
   CardAction,
@@ -8,40 +8,85 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import { Trash2Icon } from "lucide-react";
+import { CirclePlusIcon, Trash2Icon } from "lucide-react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
+import TaskCard from "./TaskCard";
 
 interface Props {
   column: Column;
   deleteColumn: (id: Id) => void;
+  updateColumn: (id: Id, title: string) => void;
+  createTask: (columnId: Id) => void;
+  tasks: Task[];
 }
 
-const ColumnContainer = ({ column, deleteColumn }: Props) => {
-  const { setNodeRef, attributes, listeners, transform, transition } =
-    useSortable({
-      id: column.id,
-      data: {
-        type: "Column",
-        column,
-      },
-    });
+const ColumnContainer = ({
+  column,
+  deleteColumn,
+  updateColumn,
+  createTask,
+  tasks,
+}: Props) => {
+  const [editMode, setEditMode] = useState(false);
+  const {
+    setNodeRef,
+    attributes,
+    listeners,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({
+    id: column.id,
+    data: {
+      type: "Column",
+      column,
+    },
+    disabled: editMode,
+  });
 
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
   };
+
+  if (isDragging) {
+    return (
+      <div
+        ref={setNodeRef}
+        style={style}
+        className="column-container opacity-40 border-2 border-rose-500"
+      ></div>
+    );
+  }
   return (
     <Card className="column-container p-0" ref={setNodeRef} style={style}>
       <CardTitle
         className="column-container-title"
         {...attributes}
         {...listeners}
+        onClick={() => setEditMode(true)}
       >
         <div className="flex items-center justify-start gap-2">
           <div className="column-container-number">0</div>
-          {column.title}
+          {!editMode && column.title}
+          {editMode && (
+            <Input
+              autoFocus
+              onBlur={() => setEditMode(false)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === "Escape") {
+                  setEditMode(false);
+                }
+              }}
+              value={column.title}
+              onChange={(e) => updateColumn(column.id, e.target.value)}
+              className="column-container-input"
+            />
+          )}
         </div>
         <CardAction>
           <Button
@@ -54,8 +99,20 @@ const ColumnContainer = ({ column, deleteColumn }: Props) => {
           </Button>
         </CardAction>
       </CardTitle>
-      <CardContent className="column-container-content">Content</CardContent>
-      <CardFooter>Footer</CardFooter>
+      <CardContent className="column-container-content">
+        {tasks.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+      </CardContent>
+      <CardFooter>
+        <Button
+          className="column-container-footer-btn"
+          onClick={() => createTask(column.id)}
+        >
+          <CirclePlusIcon />
+          Add Task
+        </Button>
+      </CardFooter>
     </Card>
   );
 };
